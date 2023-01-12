2cf83bba503dbda2ec025aa2d928a6d034e11c5f
akshatteqmaven <sakshat@teqmavens.com>
2023-01-12 17:49:27 +0530

    data

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,165 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Controller\AppController; //use is used to include the class.
+use Cake\Event\Event;
+
+/**
+ * Users Controller
+ *
+ * @property \App\Model\Table\UsersTable $Users
+ * @method \App\Model\Entity\User[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class UsersController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+        $users = $this->paginate($this->Users);  // user is model
+
+        $this->set(compact('users'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id User id.
+     * @return \Cake\Http\Response|null|void Renders view
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $user = $this->Users->get($id, [
+            'contain' => [],
+        ]);
+
+        $this->set(compact('user'));
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $user = $this->Users->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        }
+        $this->set(compact('user'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id User id.
+     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $user = $this->Users->get($id, [
+            'contain' => [],
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        }
+        $this->set(compact('user'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id User id.
+     * @return \Cake\Http\Response|null|void Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $user = $this->Users->get($id);
+        if ($this->Users->delete($user)) {
+            $this->Flash->success(__('The user has been deleted.'));
+        } else {
+            $this->Flash->error(__('The user could not be deleted. Please, try again.'));
+        }
+
+
+        return $this->redirect(['action' => 'index']);
+    }
+    //login funtin
+    public function login()
+    {
+        if ($this->request->is('post')) {
+            $user = $this->Auth->identify();
+            if ($user) {
+                $this->Auth->setUser($user);
+                return $this->redirect(['controller' => 'users']);
+            }
+            //not login
+            $this->Flash->error('Please enter your correct login credentials');
+        }
+    }
+    //logout function
+    public function logout()
+    {
+        $this->Flash->success('You are Loged out');
+        return $this->redirect($this->Auth->logout());
+    }
+
+    //registration funtion
+    public function register()
+    {
+        $user = $this->Users->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success('You are Registered and you can login now');
+                return $this->redirect(['action' => 'login']);
+            } else {
+                $this->Flash->error('Something went wrong');
+            }
+        }
+        $this->set(compact('user'));
+        $this->set('_serialzie', ['user']);
+    }
+    public function customer()
+    {
+        $user = $this->Users->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success('You are Registered and you can login now');
+            } else {
+                $this->Flash->error('Something went wrong');
+            }
+        }
+    }
+    public function beforeFilter($event)
+    {
+        $this->Auth->allow(['register']);
+    }
+    public $paginate = [
+        'limit' => 4
+    ];
+}
---
+++ templates/Users/register.php
@@ -0,0 +1,16 @@
+<br>
+<div class="index large-4 medium-4 large-offset-4 medium-offset-4 columns">
+    <div class="panel">
+        <h2 class="text-center">Registration Form</h2>
+        <?= $this->Form->create($user); ?>
+        <?= $this->Form->control('name', ['required' => false]); ?>
+        <?= $this->Form->control('phone', ['required' => false]); ?>
+        <?= $this->Form->control('email', ['required' => false]); ?>
+        <?= $this->Form->control('password', array('type' => 'password', 'required' => false)); ?>
+        <?= $this->Form->control('gender', array('type' => 'radio', 'required' => false)); ?>
+        <?= $this->Form->radio('gender', ['Male' => 'Male', 'Female' => 'Female'], ['required' => false]); ?>
+        <?= $this->Form->submit('Register', array('class' => 'button')); ?>
+        <?= $this->Form->end(); ?>
+
+    </div>
+</div>
