88fc8329407364894c07c3a2c578e120c93d1a86
akshatteqmaven <sakshat@teqmavens.com>
2023-01-17 12:45:16 +0530

    #formatting in usercontroller

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -39,10 +39,7 @@ class UsersController extends AppController
      */
     public function view($id = null)
     {
-        $user = $this->Users->get($id, [
-            'contain' => [],
-        ]);
-
+        $user = $this->Users->get($id, ['contain' => [],]);
         $this->set(compact('user'));
     }
 
