a362a35cf5446e511a29b837840d58cfeb571bf6
akshatteqmaven <sakshat@teqmavens.com>
2023-01-18 18:34:22 +0530

    #ne default made

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -34,7 +34,7 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake', 'infinite']) ?>
+    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake'/*,'infinite' */]) ?>
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
